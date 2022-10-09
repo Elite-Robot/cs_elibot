01f94ce5f69c056ef2b5bf2671a1d176794f825a
garg-akash-elibot <akashgarg@elibot.cn>
2022-10-08 20:56:22 -0700

    client file added..server needs fix

---
+++ cs_msgs/scripts/joint_move_client.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import sys
+import rospy
+from cs_msgs.srv import *
+
+def joint_move_client():
+    rospy.wait_for_service('joint_move')
+    try:
+        joint_move = rospy.ServiceProxy('joint_move', JointMove)
+        joint_ = [-1.57,-1.57,-1.57,-1.57,1.57,0]
+        acc_ = 5
+        speed_ = 1 
+        time_ = 0
+        radius_ = 0
+        resp1 = joint_move(joint_,acc_,speed_,time_,radius_)
+        print("Move joint execution status : ")
+        return resp1.result
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+ 
+if __name__ == "__main__":
+    print("Requesting move joint")
+    resp = joint_move_client()
+    print(resp)
